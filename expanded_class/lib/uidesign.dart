d3b3942f688df9f1e280ee5037f68cdf215899c2
Kefayet Ullah <kefayet107@gmail.com>
2025-10-20 11:01:18 +0600

    First commit

---
+++ expanded_class/lib/uidesign.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Uidesign extends StatefulWidget {
+  const Uidesign({super.key});
+
+  @override
+  State<Uidesign> createState() => _UidesignState();
+}
+
+class _UidesignState extends State<Uidesign> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

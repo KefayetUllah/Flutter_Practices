62139af44f1417b49b42d633b513f1f9394ae561
Kefayet Ullah <kefayet107@gmail.com>
2025-10-23 14:29:23 +0600

    Initial commit

--- expanded_class/lib/uidesign.dart
+++ expanded_class/lib/uidesign.dart
@@ -10,6 +10,112 @@ class Uidesign extends StatefulWidget {
 class _UidesignState extends State<Uidesign> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Expanded UI")),
+      body: Row(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              child: Column(
+                children: [
+                  Expanded(flex: 3, child: Container(color: Colors.teal)),
+                  Expanded(flex: 4, child: Container(color: Colors.yellow)),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Container(color: Colors.blue),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.red,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Container(color: Colors.amber),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Container(color: Colors.deepPurpleAccent),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green,
+              child: Column(
+                children: [
+                  Expanded(child: Container(color: Colors.cyanAccent)),
+                  Expanded(
+                    child: Container(
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              color: Colors.green,
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    flex: 5,
+                                    child: Container(color: Colors.amber),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(color: Colors.green),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              color: Colors.teal,
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(color: Colors.green),
+                                  ),
+                                  Expanded(
+                                    flex: 4,
+                                    child: Container(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
